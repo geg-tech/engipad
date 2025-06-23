e6896ec35ca5f966b755f132e2bd86fc646437e1
hunk egg <123428389+hunkegg@users.noreply.github.com>
2025-06-23 19:59:54 -0400

    Add files via upload

---
+++ Firmware/config.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#ifdef RGBLIGHT_ENABLE
+    #define RGBLIGHT_LED_COUNT 8 
+
+    #define RGBLIGHT_EFFECT_RGB_TEST
+
+    #define RGBLIGHT_LIMIT_VAL 225
+    #define RGBLIGHT_HUE_STEP 10
+    #define RGBLIGHT_SAT_STEP 17
+    #define RGBLIGHT_VAL_STEP 17
+#endif
