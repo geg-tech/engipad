a92f65a8823641a534ffdb2aedc64b6fc5b7bc37
hunk egg <123428389+hunkegg@users.noreply.github.com>
2025-06-23 19:22:35 -0400

    Add files via upload

---
+++ Firmware/keymap.c
@@ -0,0 +1,31 @@
+// Copyright 2023 QMK
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#include QMK_KEYBOARD_H
+
+enum custom_keycodes {
+    REDO = SAFE_RANGE,
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    switch (keycode) {
+    case REDO:
+        if (record->event.pressed) {
+            // when keycode REDO is pressed
+            SEND_STRING(SS_LCTL("y"));
+
+        } else {
+            // when keycode REDO is released
+        }
+        break;
+    }
+    return true;
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+    [0] = LAYOUT(
+        KC_UNDO,   KC_K,   KC_P   REDO,
+        KC_M,   KC_Z,   KC_LEFT_SHIFT,   KC_E
+    )
+};
---
+++ Firmware/rules.mk
@@ -0,0 +1,11 @@
+# activate RGB and OLED
+RGBLIGHT_ENABLE = yes
+RGB_MATRIX_ENABLE = no
+
+OLED_ENABLE = yes
+OLED_DRIVER = ssd1306
+OLED_TRANSPORT = i2c
+
+
+
+# bwa
